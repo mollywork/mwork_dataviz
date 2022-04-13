031c3ab9d2f126e598a0001fa83c2d55071bef23
mollywork <70611335+mollywork@users.noreply.github.com>
2022-04-12 21:44:45 -0500

    index

--- linechart_hw_4.11/javascript.js
+++ linechart_hw_4.11/javascript.js
@@ -48,7 +48,7 @@ var myChart = new Chart(ctx, {
             },
             subtitle: {
                 display: true,
-                text: 'According to Gapminder's data, the United States has had the highest life expectancy from 1800 to the present. dog'
+                text: 'Meanwhile India has had the lowest life expectancy from 1800 to the present.'
             }
         },
         scales: {
